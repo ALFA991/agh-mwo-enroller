9f7e54caacbc3dc6e585206625e35845a67d54c8
ALFA991 <csalfa991@gmail.com>
2022-05-27 17:01:53 +0200

    First step

--- src/main/java/com/company/enroller/controllers/ParticipantRestController.java
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -1,13 +1,12 @@
 package com.company.enroller.controllers;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.ParticipantService;
@@ -24,5 +23,45 @@ public class ParticipantRestController {
 		Collection<Participant> participants = participantService.getAll();
 		return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
 	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getParticipant(@PathVariable("id") String login) {
+		Participant participant = participantService.findByLogin(login);
+		if (participant == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+	}
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<?> registerParticipant(@RequestBody Participant participant){
+		Participant existPart = participantService.findByLogin(participant.getLogin());
+		if (existPart != null) {
+			return new ResponseEntity("Unable to create. A participant with login " + participant.getLogin() + " already exist.", HttpStatus.CONFLICT);
+		}else{
+			participantService.createNew(participant);
+			return new ResponseEntity<>("User " + participant.getLogin() + " created", HttpStatus.CREATED);
+		}
+	}
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> deleteParticipant(@PathVariable("id") String login){
+		Participant existPart = participantService.findByLogin(login);
+		if (existPart == null) {
+			return new ResponseEntity("Unable to delete. A participant with login " + login + " don't exist.", HttpStatus.CONFLICT);
+		}else{
+			participantService.delete(existPart);
+			return new ResponseEntity<>("User " + login + " deleted", HttpStatus.CREATED);
+		}
+	}
+	@RequestMapping(value = "", method = RequestMethod.PUT)
+	public ResponseEntity<?> updateParticipant(@RequestBody Participant participant){
+		Participant existPart = participantService.findByLogin(participant.getLogin());
+		if (existPart == null) {
+			return new ResponseEntity("Unable to update. A participant with login " + participant.getLogin() + " don't exist.", HttpStatus.CONFLICT);
+		}else{
+			participantService.updatePart(participant);
+			return new ResponseEntity<>("User " + participant.getLogin() + " updated", HttpStatus.CREATED);
+		}
+	}
+
 
 }
--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +21,28 @@ public class ParticipantService {
 		String hql = "FROM Participant";
 		Query query = connector.getSession().createQuery(hql);
 		return query.list();
+	}
+
+	public Participant findByLogin(String login){
+		String hql = "FROM Participant p WHERE p.login ='"+login+"'";
+		Query<Participant> query = connector.getSession().createQuery(hql);
+		return query.uniqueResult();
+	}
+	public void createNew(Participant participant){
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+	}
+	public void delete(Participant participant){
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();
+	}
+	public void updatePart(Participant participant){
+
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().merge(participant);
+		transaction.commit();
 	}
 
 }
